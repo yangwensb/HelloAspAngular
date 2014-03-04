c5f623b7a15913d4e2330878cc6f7a83acd610e6
mochaware <mocha@mochaware.jp>
2014-03-04 16:20:49 +0900

    remove unused property

--- HelloAspAngular.Web/Api/ResourceModels/TodoListResourceModel.cs
+++ HelloAspAngular.Web/Api/ResourceModels/TodoListResourceModel.cs
@@ -1,5 +1,4 @@
 ﻿using HelloAspAngular.Domain.TodoLists;
-using HelloAspAngular.Common;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,10 +7,9 @@ using System.Web;
 
 namespace HelloAspAngular.Web.Api.ResourceModels
 {
-    public class TodoListResourceModel: IVersioned
+    public class TodoListResourceModel
     {
         public int Id { get; set; }
-        public byte[] EntityVersion { get; set; }
         public string Name { get; set; }
         public TodoListKind Kind { get; set; }
     }
