278aa897bce932ad91f552c1a4110fbfb539534d
mochaware <mocha@mochaware.jp>
2014-03-31 23:26:59 +0900

    impl

--- HelloAspAngular.App/TodoListAppService.cs
+++ HelloAspAngular.App/TodoListAppService.cs
@@ -25,52 +25,68 @@ namespace HelloAspAngular.App
 
         public async Task<AddTodoAsyncResult> AddTodoAsync(EntityDescriptor todoListDesc, Todo todo)
         {
-            var list = new TodoList()
+            using (var tran = _unitOfWork.BeginTransaction())
             {
-                Id = todoListDesc.Id,
-                EntityVersion = todoListDesc.EntityVersion,
-            };
+                var list = new TodoList()
+                {
+                    Id = todoListDesc.Id,
+                    EntityVersion = todoListDesc.EntityVersion,
+                };
 
-            _todoListRepository.Attach(list);
-            list.Todos.Add(todo);
+                _todoListRepository.Attach(list);
+                list.Todos.Add(todo);
 
-            var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
-            await _unitOfWork.SaveAsync();
+                var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
+                await _unitOfWork.SaveAsync();
+                tran.Commit();
 
-            return new AddTodoAsyncResult(new EntityDescriptor(prepared), todo);
+                return new AddTodoAsyncResult(new EntityDescriptor(prepared), todo);
+            }
         }
 
         public async Task<EntityDescriptor> UpdateTodoAsync(EntityDescriptor todoListDesc, Todo todo)
         {
-            var storedList = await _todoListRepository.FindAsync(l => l.Id == todoListDesc.Id, new[] { "Todos" });
-            storedList.ChangeTodo(todo);
+            using (var tran = _unitOfWork.BeginTransaction())
+            {
+                var storedList = await _todoListRepository.FindAsync(l => l.Id == todoListDesc.Id, new[] { "Todos" });
+                storedList.ChangeTodo(todo);
 
-            var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
-            await _unitOfWork.SaveAsync();
+                var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
+                await _unitOfWork.SaveAsync();
+                tran.Commit();
 
-            return new EntityDescriptor(prepared);
+                return new EntityDescriptor(prepared);
+            }
         }
 
         public async Task<EntityDescriptor> ArchiveAsync(EntityDescriptor todoListDesc)
         {
-            var storedList = await _todoListRepository.FindAsync(l => l.Id == todoListDesc.Id, new[] { "Todos" });
-            await _todoListService.ArchiveAsync(storedList);
+            using (var tran = _unitOfWork.BeginTransaction())
+            {
+                var storedList = await _todoListRepository.FindAsync(l => l.Id == todoListDesc.Id, new[] { "Todos" });
+                await _todoListService.ArchiveAsync(storedList);
 
-            var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
-            await _unitOfWork.SaveAsync();
+                var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
+                await _unitOfWork.SaveAsync();
+                tran.Commit();
 
-            return new EntityDescriptor(prepared);
+                return new EntityDescriptor(prepared);
+            }
         }
 
         public async Task<EntityDescriptor> ClearTodosAsync(EntityDescriptor todoListDesc)
         {
-            var storedList = await _todoListRepository.FindAsync(l => l.Id == todoListDesc.Id, new[] { "Todos" });
-            await _todoListService.ClearTodosAsync(storedList);
+            using (var tran = _unitOfWork.BeginTransaction())
+            {
+                var storedList = await _todoListRepository.FindAsync(l => l.Id == todoListDesc.Id, new[] { "Todos" });
+                await _todoListService.ClearTodosAsync(storedList);
 
-            var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
-            await _unitOfWork.SaveAsync();
+                var prepared = _todoListRepository.PrepareVersioning(todoListDesc);
+                await _unitOfWork.SaveAsync();
+                tran.Commit();
 
-            return new EntityDescriptor(prepared);
+                return new EntityDescriptor(prepared);
+            }
         }
     }
 }
